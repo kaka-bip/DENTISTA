5d79b83308ac239bed17b359746ddfaf2d1169ec
KAUAN_DEV⌨️ <contatemporaria6565@gmail.com>
2025-12-19 22:26:38 -0300

    ..

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,10 @@ function marcar(Restauração) {
   let url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
   window.open(url, "_blanck");
 }
+
+function marcar(aparelho){
+  let telefone = "5575991890654"
+  let mensagem = `Olá, boa tarde! Gostaria de marcar um horário para fazer o tratamento:  ${aparelho}`
+  let url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
+  window.open(url, '_blank')
+}
